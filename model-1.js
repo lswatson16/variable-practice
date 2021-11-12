b9eb4cf874a10cb2e0b3a1d51d3d75311742eabc
Lauralyn Watson <lauralynw16@gmail.com>
2021-11-12 16:52:35 -0500

    Add object variable and fixed syntax errors

--- model-1.js
+++ model-1.js
@@ -1,8 +1,20 @@
-var name = "Harvey"
-var pet = "Fluffy"
-var age = 5
-var luckyNumber = 21
-var isLucky = true
-var isHappy = true
+var name = "Harvey";
+var pet = "Fluffy";
+var age = 5;
+var luckyNumber = 21;
+var isLucky = true;
+var isHappy = true;
 var friends = ["Phil", "Lil", "Tommy", "Chucky"];
 var fruits = ["apple", "pear", "pineapple", "banana"];
+var dog = {
+  name: "Fluffy",
+  favoriteToy: "red ball",
+  friends: ["Molly", "Polly"]
+};
+// console.log(dog);
+
+var cat = {
+  name: "Kitty",
+  isFriendly: false,
+};
+// console.log(cat);
